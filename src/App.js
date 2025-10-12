57845b3c5504b695a0eb4d1b45726c282431c705
Rohit89205 <rohit892051182@gmail.com>
2025-10-12 19:41:22 +0530

    done all chnages

--- src/App.js
+++ src/App.js
@@ -9,7 +9,6 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import MainNavigation from "./components/Navigation/MainNavigation";
 import MobileNavbar from "./components/Navigation/MobileNavbar";
-import Topbar from "./components/Topbar/Topbar";
 import ScrollToTop from "./components/ScrollTop";
 import Footer from "./components/Footer";
 import VisaApplicationForm from "./components/VisaApplicationForm";
@@ -21,19 +20,18 @@ import Apply3 from "./components/FormPages/Apply3";
 function App() {
 
   return (
-      <BrowserRouter>
-  
+    <BrowserRouter>
+
       <ToastContainer />
       <ScrollToTop />
-      {/* <Topbar /> */}
       <MainNavigation />
       <MobileNavbar />
       <Routes>
         <Route exact path="/" element={<Home />} />
         <Route exact path="/Apply" element={<VisaApplicationForm />} />
         <Route exact path="/Apply/:id" element={<VisaApplicationForm />} />
-        <Route exact path="/Apply2/:id" element={<Apply2/>} />
-           <Route path="/Apply3" exact component={Apply3} />
+        <Route exact path="/Apply2/:id" element={<Apply2 />} />
+        <Route path="/Apply3/:id" exact component={<Apply3 />} />
         <Route exact path="/partial-Filled" element={<PartialFilled />} />
         <Route exact path="/contact" elemenmt={<Contact />} />
       </Routes>
--- src/components/PartialFilled.js
+++ src/components/PartialFilled.js
@@ -1,61 +1,62 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
-const ApplicationForm = () => {
-  const [applicationNumber, setApplicationNumber] = useState('');
-  const [status , setStatus] = useState(false);
+import { useNavigate } from 'react-router-dom';
 
-  const  checkStatus = ( data)=>{
-       console.log(data, "This is my data"); 
-       setApplicationNumber(data);
-       setStatus(true );
+const ApplicationForm = () => {
+  const navigate = useNavigate();
+  const [applicationNumber, setApplicationNumber] = useState('');
+  const [status, setStatus] = useState(false);
+
+  const checkStatus = (data) => {
+    console.log(data, "This is my data");
+    setApplicationNumber(data);
+    setStatus(true);
   }
 
   const handleSubmit = (e) => {
     e.preventDefault();
     console.log('Application Number:', applicationNumber);
+    navigate(`/apply/${applicationNumber}`)
     setStatus(false)
   };
 
   return (
-      <section>
-        <div className="container partial-formContainer">
-         <div className='row'>
-            <div className='col-12'>
-                 <form className="application-form" onSubmit={handleSubmit}>
-        <h2 className="form-title">Application Details</h2>
-        
-        <div className="form-group flex">
-           <div>
-             <label htmlFor="applicationNumber" className="form-label">
-            Enter your Application Number : 
-          </label>
-          </div>
-            <div>
-               <input
-            type="text"
-            id="applicationNumber"
-            className="form-control form-input"
-            value={applicationNumber}
-            onChange={(e) => checkStatus(e.target.value)}
-            placeholder="Enter application number"
-            required
-          />
-            </div>
-        </div>
+    <section>
+      <div className="container partial-formContainer">
+        <div className='row'>
+          <div className='col-12'>
+            <form className="application-form" onSubmit={handleSubmit}>
+              <h2 className="form-title">Application Details</h2>
 
-        {status && (
-        //    <Link to={`/Apply/${applicationNumber}`}>
-        <Link to='/Apply3'>
-            <button type="submit" className="submit-buttonPartial">
-            Submit
-            </button>
-           </Link>
-        )}
-      </form>
-            </div>
-         </div>
-    </div>
-      </section>
+              <div className="form-group flex">
+                <div>
+                  <label htmlFor="applicationNumber" className="form-label">
+                    Enter your Application Number :
+                  </label>
+                </div>
+                <div>
+                  <input
+                    type="text"
+                    id="applicationNumber"
+                    className="form-control form-input"
+                    value={applicationNumber}
+                    onChange={(e) => checkStatus(e.target.value)}
+                    placeholder="Enter application number"
+                    required
+                  />
+                </div>
+              </div>
+
+              {/* {status && ( */}
+                <button type="submit" className="submit-buttonPartial">
+                  Submit
+                </button>
+              {/* )} */}
+            </form>
+          </div>
+        </div>
+      </div>
+    </section>
   );
 };
 
