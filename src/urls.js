171443f91aafb1b0e941a980a845e6a80e386e77
Rohit89205 <rohit892051182@gmail.com>
2025-10-25 16:18:08 +0530

    lined

--- src/urls.js
+++ src/urls.js
@@ -3,13 +3,13 @@ import axios from 'axios';
 // for local development 
 // export const BASE_URL = "http://192.168.1.34:5000/api/";
 // export const ImageUrl = "http://192.168.1.34:5000/file/images/";
-// export const BASE_URL = "http://localhost:5000/api/";
-// export const ImageUrl = "http://localhost:5000/file/images/";
+export const BASE_URL = "http://localhost:4000/api/";
+export const ImageUrl = "http://localhost:4000/file/images/";
 
 
 /// FOR QAS
-export const BASE_URL = "https://testapi.bharatprinthub.com/api/";
-export const ImageUrl = "https://testapi.bharatprinthub.com/file/images/";
+// export const BASE_URL = "https://testapi.bharatprinthub.com/api/";
+// export const ImageUrl = "https://testapi.bharatprinthub.com/file/images/";
 
 
 const TOKEN = localStorage.getItem("UserDetailToken");
